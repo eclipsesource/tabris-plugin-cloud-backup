a0472b0b15c6abe2b79831ffdf158aceb9076012
Christian Petrov <cpetrov@eclipsesource.com>
2020-04-09 13:22:54 +0200

    Update example to Tabris.js 3.4
    Move example to .example to enable builds that were previously not
    possible due to a Cordova limitation [1]. Note that the example build
    only works with Tabris CLI <= 3.2 since it does not work with the newer
    Cordova versions supplied with Tabris CLI 3.3+. Long-term solution
    pending.
    
    [1]: apache/cordova-cli#401
    
    Change-Id: Ic2506a9b4224355128b91a86a1ca643b88ce1778

--- .example/src/app.js
+++ .example/src/app.js
@@ -1,16 +1,16 @@
-const {Button, TextInput, TextView, ui, localStorage} = require('tabris');
+const {Button, TextInput, TextView, contentView, localStorage} = require('tabris');
 
 const KEY = 'snippet-value';
 
 let valueLabel = new TextView({
   left: 32, top: 32,
   text: 'Value:'
-}).appendTo(ui.contentView);
+}).appendTo(contentView);
 
 let valueField = new TextInput({
   left: 'prev() 12', baseline: valueLabel, right: 32,
   text: localStorage.getItem(KEY) || 'Hello world!'
-}).appendTo(ui.contentView);
+}).appendTo(contentView);
 
 new Button({
   left: 24, right: '66%', top: [valueLabel, 24],
@@ -18,14 +18,14 @@ new Button({
 }).on('select', () => {
   localStorage.setItem(KEY, valueField.text);
   valueField.text = '';
-}).appendTo(ui.contentView);
+}).appendTo(contentView);
 
 new Button({
   left: '33% 12', right: '33% 12', top: [valueLabel, 24],
   text: 'Get'
 }).on('select', () => {
   valueField.text = localStorage.getItem(KEY) || '';
-}).appendTo(ui.contentView);
+}).appendTo(contentView);
 
 new Button({
   left: '66%', right: 24, top: [valueLabel, 24],
@@ -33,4 +33,4 @@ new Button({
 }).on('select', () => {
   localStorage.removeItem(KEY);
   valueField.text = '';
-}).appendTo(ui.contentView);
+}).appendTo(contentView);
